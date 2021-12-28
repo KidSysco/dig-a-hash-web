029a71c840b0ac38f957c9c11e9d4386a865387d
Ryan Segura <>
2021-12-27 20:06:59 -0600

    Added Tailwind

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,20 @@
   </div>
 </template>
 
+<script>
+
+export default {
+	name: 'app'
+}
+</script>
+
+
 <style>
 body,
 html {
   background-color: black;
+  height: 100%;
+  padding: 0;
+  margin: 0;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="home">
-    <img src="Dig-A-Hash.jpg" class="logo" />
+
+  <div class="">
+    <img src="Dig-A-Hash.jpg" class="" />
 	<div class="copyright">&copy; Dig-A-Hash 2022</div>
-  </div>
+  </div> 
 </template>
 
 <script>
@@ -15,19 +16,4 @@ export default {
 }
 </script>
 <style scoped>
-.home {
-  text-align: center;
-}
-
-.logo {
-  margin-top: 30px;
-  width: 50%;
-}
-
-.copyright {
-	text-align: center;
-	font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-	color:gray;
-	font-size: 10px;
-}
 </style>
