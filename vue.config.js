d7e00669f039c8045bbe22ad3700c4bd0861cad3
Ryan Segura <>
2022-01-15 15:35:26 -0600

    Removed goffy workboxpluginmode from vue config

--- vue.config.js
+++ vue.config.js
@@ -5,14 +5,6 @@ module.exports = {
     themeColor: '#000000',
     msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-
-    // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'dev/sw.js',
-      // ...other Workbox options...
-    }
+    appleMobileWebAppStatusBarStyle: 'black'
   }
 }
